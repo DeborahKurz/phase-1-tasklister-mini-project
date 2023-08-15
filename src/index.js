507be81f596f9c272abd5e5145e17e8c778d3a2a
Deborah Kurz <DeborahKurz6@gmail.com>
2023-08-15 13:44:40 -0600

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTasks(e.target.newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function buildTasks(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', causeDelete)
+  btn.textContent = 'x'
+  p.textContent = `${toDo}  `
+  p.appendChild(btn)
+  document.querySelector("#list").appendChild(p)
+}
+
+function causeDelete(e){
+  e.target.parentNode.remove()
+}
